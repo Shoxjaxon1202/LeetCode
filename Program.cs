8a76566ca684ead8dcd08f374f59bdf203a4072e
Shoxjaxon1202 <shoxjaxonabdusalimov1202@gmail.com>
2024-09-20 20:44:28 +0500

    Palindrom

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿System.Console.WriteLine("Salom");
+﻿// class Program
+// {
+//     public static void Main()
+//     {
+//         int[] numbers = new int[]{
+//             2,7,11,15
+//         };
+
+//         int target = 9;
+
+//         int result = numbers[0];
+//         for(int i = 0; i < numbers.Length; i++)
+//         {
+//             if(numbers[i] == result)
+//             {
+//                 result += numbers[i];
+//             }
+//         }
+//         System.Console.WriteLine(result);
+//         for(int i = 0; i < numbers.Length; i++)
+//         {
+//             if(numbers[i].ToString().Contains(result.ToString()))
+//             {
+//                 System.Console.Write(i + " ");
+//             }
+//         }
+//     }
+// }
+
+
+
+
